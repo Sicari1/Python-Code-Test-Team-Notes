a1eb3bff0201afed78501b839d8b63d481eb92ac
SeongJin Kim <45029583+Sicari1@users.noreply.github.com>
2024-09-23 13:29:47 +0900

    Update readline.py

--- basics/readline.py
+++ basics/readline.py
@@ -1,1 +1,16 @@
+# Basic readline
+## Example 1
+a, b, c = map(int, input().split())
 
+## Example 2
+n = int(input))
+data = list(map(int, input().split()))
+
+print(n)
+print(data)
+              
+# Faster
+## Example 1
+import sys
+data = sys.stdin.readline().rstrip()
+print(data)
