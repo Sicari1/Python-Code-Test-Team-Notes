3e6a4d3831a55045b984faffa534c350296007d2
SeongJin Kim <45029583+Sicari1@users.noreply.github.com>
2024-09-23 13:51:04 +0900

    Update Permutation & Combination.py

--- basics/Permutation & Combination.py
+++ basics/Permutation & Combination.py
@@ -11,3 +11,17 @@ data = ['A', 'B', 'C']
 
 result = list(combinations(data,3))
 print(result)
+
+# 중복 순열
+from itertools import product
+data = ['A', 'B', 'C']
+
+result = list(product(data, repeat=2))
+print(result)
+
+# 중복 조합
+from itertools import combinations_with_replacement
+data = ['A', 'B', 'C']
+
+result = list(combinations_with_replacement(data, 2)
+print(result)
