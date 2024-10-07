2afd098d1764662462815fa9f1018cbb4a0d4695
SeongJin Kim <45029583+Sicari1@users.noreply.github.com>
2024-10-07 23:30:43 +0900

    Update DFS.py

--- Searching/DFS.py
+++ Searching/DFS.py
@@ -1,12 +1,12 @@
 # DFS 메서드 정의
 def dfs(graph, v, visited):
-  # 현재 노드를 방문 처리
-  visited[v] = True
-  print(v, end=' ')
-  # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
-  for i in graph[v]:
-    if not visited[i]:
-      dfs(graph, i, visited)
+	# 현재 노드를 방문 처리
+	visited[v] = True
+	print(v, end=' ')
+	# 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+	for i in graph[v]:
+		if not visited[i]:
+			dfs(graph, i, visited)
 
 graph = [
   [], # 그래프가 1번부터 시작하는 경우가 많기 때문에 0번은 보통 비워둠.
