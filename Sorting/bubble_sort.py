a3656ba3ae5b6f590a6fada0d2887f5ceb149bec
SeongJin Kim <45029583+Sicari1@users.noreply.github.com>
2024-10-07 16:43:44 +0900

    Create bubble_sort.py

---
+++ Sorting/bubble_sort.py
@@ -0,0 +1,14 @@
+# bubble_sort.py
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+# 예시 코드
+if __name__ == "__main__":
+    arr = [64, 34, 25, 12, 22, 11, 90]
+    sorted_arr = bubble_sort(arr)
+    print("정렬된 배열:", sorted_arr)
